d5b9f4c6dc43cd2a4351e3e886967bb11c088a7a
jerem <114385056+Jerem-EstIcI@users.noreply.github.com>
2023-11-09 11:42:29 +0100

    floating text

---
+++ Generateur/Entite/armor-stand-floating-text.js
@@ -0,0 +1,20 @@
+function generateCommand(){
+    const spawnType = document.getElementById("spawnType").value;
+    const spawnX = document.getElementById("spawnX").value;
+    const spawnY = document.getElementById("spawnY").value;
+    const spawnZ = document.getElementById("spawnZ").value;
+    const CustomName = document.getElementById("CustomName").value;
+    const CustomNameColor = document.getElementById("CustomNameColor").value;
+
+    let command = `/summon armor_stand ${spawnType === 'relative' ? `~${spawnX} ~${spawnY} ~${spawnZ}` : `${spawnX} ${spawnY} ${spawnZ}`} {Invisible:1b,Invulnerable:1b,NoGravity:1b,Marker:1b,CustomName:"{\"text\":\"${CustomName}\",\"color\":\"${CustomNameColor}\"}",CustomNameVisible:1b}`;
+
+    const commandResult = document.getElementById("commandResult");
+    commandResult.value = command;
+}
+
+function copyCommand() {
+    const commandResult = document.getElementById("commandResult");
+    commandResult.select();
+    document.execCommand("copy");
+    alert("Commande copiée !");
+}
